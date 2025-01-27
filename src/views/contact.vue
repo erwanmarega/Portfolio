9ab2d0e5076560f64ed4ef7765e14f4aa72d7e58
Erwan Marega <maregaerwan@gmail.com>
2025-01-27 22:28:53 +0100

    Copyright

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -34,7 +34,7 @@
   
 	  <div class="absolute bottom-4 left-4 text-white text-sm font-light">
 		<p>
-		  <a href="/mentions-legales" class="hover:underline">Mentions légales</a> | &copy; 2025
+		 &copy; 2025 Erwan Marega | Tous droits réservés
 		</p>
 	  </div>
   	  <div class="absolute bottom-4 right-4 text-white text-xl font-bold">
