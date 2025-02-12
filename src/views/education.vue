c8c28726f1335cfdce7a62e9fe87da0cb1fb440a
Erwan Marega <maregaerwan@gmail.com>
2025-02-12 10:27:41 +0100

    Changement master en Mastère

--- src/views/education.vue
+++ src/views/education.vue
@@ -51,7 +51,7 @@
               "Formation polyvalente en multimédia, web, communication et développement web."
           },
           {
-            title: "Mastère en Développement Web",
+            title: "Mastère Dev Manager Full Stack",
             institution: "Efrei",
             years: "2025 - 2027",
             description:
