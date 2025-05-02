f147fe21e1f80e65908357c5cadbb058ddd53f0c
Erwan Marega <maregaerwan@gmail.com>
2025-05-02 23:21:19 +0200

    MAJ landing page

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import './assets/main.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBots } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import "animate.css";
 
 library.add(faBots);
 
